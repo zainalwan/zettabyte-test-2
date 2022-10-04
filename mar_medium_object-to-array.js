3797f5c792a9aacfc4f1a48a40c98a7645235ecc
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2022-10-05 00:00:56 +0700

    Convert profit data structure

---
+++ mar_medium_object-to-array.js
@@ -0,0 +1,38 @@
+/**
+ * Direction:
+ * Convert this data of profit per days from object into array of objects
+ *
+ * Expected Result:
+ * [ { date: '12/25/21', profit: 400000 },
+ *   { date: '12/26/21', profit: 200000 },
+ *   { date: '12/27/21', profit: 450000 },
+ *   { date: '12/28/21', profit: 500000 },
+ *   { date: '12/29/21', profit: 420000 },
+ *   { date: '12/30/21', profit: 420000 },
+ *   { date: '12/31/21', profit: 400000 } ]
+ */
+
+const reports = {
+    '12/25/21': 400000,
+    '12/26/21': 200000,
+    '12/27/21': 450000,
+    '12/28/21': 500000,
+    '12/29/21': 420000,
+    '12/30/21': 420000,
+    '12/31/21': 400000
+}
+
+function result(reports) {
+    let result = [];
+
+    for (key in reports) {
+        result.push({
+            date: key,
+            profut: reports[key],
+        });
+    }
+
+    return result;
+}
+
+console.log(result(reports));
